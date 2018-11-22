cb7f3f0eae03f4a2caeea87be3d592631cc3946f
Minthasof <847059161@qq.com>
2018-11-22 18:46:09 +0800

    首页获取数据成功

--- src/main.js
+++ src/main.js
@@ -1,8 +1,37 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+// 下载 VueRouter
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+
+
+
+// 导入 全局的样式
+import "./assets/site/css/style.css";
+
+// 导入每一个页面的 组件
+import index from './components/index.vue'
+
+// 实例化路由规则
+let routes = [
+  { path: "/", redirect: "/index" },
+  {
+    path:'/index',
+    component:index
+  }
+
+];
+
+// 实例化VueRouter
+let router = new VueRouter({
+  routes
+});
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  // 路由选项
+  router
+}).$mount("#app");
